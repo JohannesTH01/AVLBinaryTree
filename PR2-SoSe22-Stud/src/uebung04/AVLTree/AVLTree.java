3e216c5b7cd8f84a6a4271765077007347b16cb2
Johannes <johannes.t1912>
2022-06-20 15:06:18 +0200

    insert Methode angepasst und Rotationen implementiert

--- PR2-SoSe22-Stud/src/uebung04/AVLTree/AVLTree.java
+++ PR2-SoSe22-Stud/src/uebung04/AVLTree/AVLTree.java
@@ -11,83 +11,45 @@ public class  AVLTree extends BinaryTree {
 
     @Override
     public boolean insert(Comparable elem){
-        if(this.root == null){
-            AVLTreeNode newNode = new AVLTreeNode(elem);
-            newNode.parent = null;
-            this.root = newNode;
-            return true;
-        }
-
-        return this.insertR((AVLTreeNode) this.root, new AVLTreeNode(elem), 0);
-    }
-
-    public boolean insertR(AVLTreeNode parentNode, AVLTreeNode newNode, int recursiondepth){
-        int comparisonResult = newNode.getElement().compareTo(parentNode.getElement());
-        println(comparisonResult);
-
-        if(newNode.getElement().compareTo(parentNode.getElement()) == 1) {
-            if(parentNode.getRight() == null){
-                println(newNode.getElement() + " inserted right");
-                newNode.parent = parentNode;    //ref to parent for recursive balance check
-                parentNode.setRight(newNode);
-            }else{
-                this.insertR((AVLTreeNode) parentNode.getRight(), newNode, recursiondepth++);
-            }
-        }
-
-        if(newNode.getElement().compareTo(parentNode.getElement()) == -1){
-            if(parentNode.getLeft() == null){
-                println(newNode.getElement() + " inserted left");
-                newNode.parent = parentNode;    //ref to parent for recursive balance check
-                parentNode.setLeft(newNode);
-            }else{
-                this.insertR((AVLTreeNode) parentNode.getLeft(), newNode, recursiondepth++);
-            }
-        }else{
-            return false;   //element already inserted
-        }
-
-        if(recursiondepth == 0)
-            checkBalanceDebug(newNode);
+        this.root = insertR((AVLTreeNode) this.root, new AVLTreeNode(elem));
         return true;
     }
 
-    private void checkBalanceDebug(AVLTreeNode currentNode){
-        currentNode.calculateBalance();
-        if(!currentNode.isBalanced) {
-            println("current node: " + currentNode.getElement() + " rebalance needed");
-            //now we have to decide which type of rotation is needed
+    private AVLTreeNode insertR(AVLTreeNode node, AVLTreeNode dataToInsert){
+        if(node == null)
+            return dataToInsert;
+        if(dataToInsert.getElement().compareTo(node.getElement()) < 0)
+            node.setLeft(insertR((AVLTreeNode) node.getLeft(), dataToInsert));
+        else if(dataToInsert.getElement().compareTo(node.getElement()) > 0)
+            node.setRight(insertR((AVLTreeNode) node.getRight(), dataToInsert));
 
-            //right rotation (for testing purposes !)
-            Comparable tempValue = currentNode.getElement();
-            currentNode.parent.setLeft(currentNode.getLeft());
-            currentNode.parent.getLeft().setRight(new AVLTreeNode(tempValue));
-
-
-            return;
-
-        }
-        else
-            println("current node: " + currentNode.getElement());
-        if(currentNode.parent != null)
-            checkBalanceDebug(currentNode.parent);
-
-    }
-    private void checkBalance (AVLTreeNode node){
-        //int this method the balance of the previously inserted node will be checked.
-        //Afterwards the method checks the tree recursively for further imbalanced nodes
-        int balanceDifferece = node.calculateBalance();
-        println("Node: " +node.getElement() + " is balances ?" + node.isBalanced);
-        if(balanceDifferece > 1 || balanceDifferece < -1 && !node.isBalanced){
-            //rebalance
-            rebalance(node,balanceDifferece);
-        }
-        if(node.parent != null)
-            checkBalance(node.parent);
+        return checkForRotation(node);
     }
 
+    private AVLTreeNode checkForRotation(AVLTreeNode currentNode){
+        //calculate balance
+        int balance = currentNode.calculateBalance();
+
+        //left-heavy
+        if(balance > 1){
+            //do rightrotation
+            if(((AVLTreeNode)currentNode.getLeft()).calculateBalance() < 0)
+                currentNode.setLeft(rotateLeft((AVLTreeNode) currentNode.getLeft()));
+
+
+            return rotateRight(currentNode);
+        }
+
+        //right-heavy
+        if(balance < 1){
+            if(((AVLTreeNode)currentNode.getRight()).calculateBalance() > 0)
+                currentNode.setRight(rotateRight((AVLTreeNode) currentNode.getRight()));
+            return rotateLeft(currentNode);
+        }
+
+        return currentNode;
+    }
     private void rebalance(AVLTreeNode node, int balanceDifferece){
-
         if(balanceDifferece > 1){
             if(node.getRight()!= null && node.getLeft() !=null)
                 node.setLeft(rotateLeft(node));
